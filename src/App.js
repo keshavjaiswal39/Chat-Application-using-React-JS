074da9ea831afd4da729c5f605ddc36fd8cb6bf0
Keshav Jaiswal <keshav.jaiswal39@gmail.com>
2021-05-29 14:07:56 +0530

    Completed the chat application

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
- import { ChatEngine } from 'react-chat-engine';
+import { ChatEngine } from 'react-chat-engine';
 
- import './App.css';
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+
+import './App.css';
 
 const App = () => {
-    return (
-        <ChatEngine
-            height='100vh'
-            projectID="77fc5f33-c9f5-439d-adbb-130480df035e"
-            userName="keshav.jaiswal"
-            userSecret="playboy123"
-            
-        />
-    )
-}
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID="77fc5f33-c9f5-439d-adbb-130480df035e"
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
+  );
+};
+
+// infinite scroll, logout, more customizations...
 
 export default App;
-
-
- 

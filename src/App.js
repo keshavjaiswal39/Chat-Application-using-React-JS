a9e480e79909c36caf00188477fc85ba794d91f5
Keshav Jaiswal <keshav.jaiswal39@gmail.com>
2021-05-29 11:33:02 +0530

    Initiated the Chat Engine

---
+++ src/App.js
@@ -0,0 +1,20 @@
+ import { ChatEngine } from 'react-chat-engine';
+
+ import './App.css';
+
+const App = () => {
+    return (
+        <ChatEngine
+            height='100vh'
+            projectID="77fc5f33-c9f5-439d-adbb-130480df035e"
+            userName="keshav.jaiswal"
+            userSecret="playboy123"
+            
+        />
+    )
+}
+
+export default App;
+
+
+ 
